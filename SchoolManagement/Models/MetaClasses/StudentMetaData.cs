a02b3eb5495a0a5e2a6a34cbb78d5c9175d8a8bd
Noah Maddox <nmaddox@thrivetech.com>
2022-01-13 16:49:06 -0500

    Add project files.

---
+++ SchoolManagement/Models/MetaClasses/StudentMetaData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.Models
+{
+    public class StudentMetaData
+    {
+        public int StudentID { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Birth Date")]
+        public DateTime? BirthDate { get; set; }
+
+        [Display(Name ="Date of Enrollment")]
+        public DateTime? EnrollmentDate { get; set; }
+
+        
+    }
+
+    [MetadataType(typeof(StudentMetaData))]
+    public partial class Student { }
+}
